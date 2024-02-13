6db24e70282d99568af83371e2cc197d3b2e944a
Brendan Poon <bpoon24@students.polytechnic.org>
2024-02-13 11:45:30 -0800

    Fields, constructor, and toString for Passenger.

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,17 @@
 public class Passenger {
-    
+    public static int IDGen = 0;
+    public int ID;
+    public int start;
+    public int end;
+
+    public Passenger(int myStart, int myEnd){
+        ID = IDGen;
+        IDGen++;
+        start = myStart;
+        end = myEnd;
+    }
+
+    public String passengerToString(){
+        return "Passenger " + ID + ": " + "Start = " + start + ", " + "End = " + end;
+    }
 }
