45ba380f3ebb8e86e5c83a155bcf80ad3a93b535
Brendan Poon <bpoon24@students.polytechnic.org>
2024-02-18 13:51:21 -0800

    Wrote findmaxcardistance method for Road.

--- Road.java
+++ Road.java
@@ -10,4 +10,20 @@ public class Road {
         rcars = new ArrayList<Car>();  
         rstations = new ArrayList<Station>();  
     }
+
+    public int findmaxcardistance(ArrayList<Car> rcars){ //The max distance is the number of "moves" that need to be called
+        int maxdistance = 0;
+        int distance = 0;
+        for(int i = 0; i < rcars.size(); i++){
+            distance = Math.abs(rcars.get(i).getcstart() - rcars.get(i).getcend());
+            if(distance > maxdistance){
+                maxdistance = distance;
+            }
+        }
+        return maxdistance;
+    }
+
+    public void moveallcars(){
+        for(int i = 0; i < rcars.size(); i++){
+    }
 }
