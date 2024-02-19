3a9c5ad3804e08d60ad0560780d3632613ffec41
Brendan Poon <bpoon24@students.polytechnic.org>
2024-02-18 16:55:00 -0800

    Wrote key methods in Road.

--- Road.java
+++ Road.java
@@ -11,6 +11,38 @@ public class Road {
         rstations = new ArrayList<Station>();  
     }
 
+    //Randomness generators
+
+    public ArrayList<Station> stationgenerator(int numstations){ //Make numstations whatever you want
+        for(int i = 0; i < numstations; i++){ //Stations start at 0
+            Station myStation = new Station(i); //Creates a Station with ID number corresponding to i
+            rstations.add(myStation);
+        }
+        return rstations;
+    }
+
+    public ArrayList<Car> cargenerator(int numcars, int numstations){ //Make numcars whatever you want.  numstations must match the stationgenerator parameter.
+        for(int i = 0; i < numcars; i++){
+            int randomcstart = (int) (Math.random() * (numstations)); //Generates a random integer from 0 to numstations - 1 (the index of the last Station)
+            int randomcend = (int) (Math.random() * (numstations));
+            Car myCar = new Car(randomcstart, randomcend);
+            rcars.add(myCar);
+        }
+        return rcars;
+    }
+
+    public ArrayList<Passenger> passengergenerator(int numpassengers, int numstations){ //Make numpassengers whatever you want.  numstations must match the stationgenerator parameter.
+        for(int i = 0; i < numpassengers; i++){
+            int randompstart = (int) (Math.random() * (numstations)); 
+            int randompend = (int) (Math.random() * (numstations));
+            Passenger myPassenger = new Passenger(randompstart, randompend);
+            rpassengers.add(myPassenger);            
+        }
+        return rpassengers;
+    }
+
+    //Helper methods for runner
+
     public int findmaxcardistance(ArrayList<Car> rcars){ //The max distance is the number of "moves" that need to be called
         int maxdistance = 0;
         int distance = 0;
@@ -25,5 +57,28 @@ public class Road {
 
     public void moveallcars(){
         for(int i = 0; i < rcars.size(); i++){
+            rcars.get(i).move(); //The move() method is in Car
+        }
+    }
+
+    public void dropoffallpassengers(){
+        for(int i = 0; i < rstations.size(); i++){
+            rstations.get(i).dropoff(); //Dropoff() is in Station
+        }
+    }
+
+    public void pickupallpassengers(){
+        for(int i = 0; i < rstations.size(); i++){
+            rstations.get(i).pickup(); //Pickup() is in Station
+        }        
+    }
+
+    //The top level method!
+    public void runner(){
+        for(int i = 0; i < findmaxcardistance(rcars); i++){
+            moveallcars();
+            dropoffallpassengers();
+            pickupallpassengers();
+        }
     }
 }
